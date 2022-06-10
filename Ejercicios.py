5a83f89eef85f846c2bfb724c9bb473e5ea1ecb3
Erick Bautista <72156877+oxjleoxo@users.noreply.github.com>
2022-06-10 10:11:13 -0500

    Textos
    Agregue nuevos textos para ver cómo funcionan

--- Ejercicios.py
+++ Ejercicios.py
@@ -36,58 +36,64 @@ if __name__ == '__main__':
 
 
 
-#el type() funciona para mostrar el tipo de variable que es una variable
-print(type("Cosas simples"))
-#el + funciona para unir cadenas de texto y en numeros los suma 
-print("bye " + "mundo")
+# #el type() funciona para mostrar el tipo de variable que es una variable
+# print(type("Cosas simples"))
+# #el + funciona para unir cadenas de texto y en numeros los suma 
+# print("bye " + "mundo")
 
-#Entero
-print(30)
-print(type(30))
+# #Entero
+# print(30)
+# print(type(30))
 
-#float o decimales
-print(30.1)
-print(type(30.1))
+# #float o decimales
+# print(30.1)
+# print(type(30.1))
 
-#listas
-lista1 = [1,2,3,4]
-print(type(lista1))
-#tuplas, son listas pero que no se pueden cambiar los datos
-dupla1 = (10,20,30,40)
-print(type(dupla1))
-
-
-#diccionarios
-diccionario1 = {
-    "Nombre de la persona":"Ryan",
-    "Apellido":"Ray",
-    "Nickname":"oxjleoxo"
-}
-print(type(diccionario1))
-
-#variable sin datos
-Nada = None
-print(type(Nada))
-#Las variables de python son case sensitive
-
-#Se pueden guardar varias variables y sus datos a la vez
-x,book = 100,"La caida de Reach"
-
-#para usar constantes tienes que usar todo el nombre de la variable en mayusculas y generalmente en otro archivo de Python para que así no se pueda cambiar
-GRAVITY = 3.14
-print(type(GRAVITY))
+# #listas
+# lista1 = [1,2,3,4]
+# print(type(lista1))
+# #tuplas, son listas pero que no se pueden cambiar los datos
+# dupla1 = (10,20,30,40)
+# print(type(dupla1))
 
 
+# #diccionarios
+# diccionario1 = {
+#     "Nombre de la persona":"Ryan",
+#     "Apellido":"Ray",
+#     "Nickname":"oxjleoxo"
+# }
+# print(type(diccionario1))
+
+# #variable sin datos
+# Nada = None
+# print(type(Nada))
+# #Las variables de python son case sensitive
+
+# #Se pueden guardar varias variables y sus datos a la vez
+# x,book = 100,"La caida de Reach"
+
+# #para usar constantes tienes que usar todo el nombre de la variable en mayusculas y generalmente en otro archivo de Python para que así no se pueda cambiar
+# GRAVITY = 3.14
+# print(type(GRAVITY))
 
 
 
-print("Cosas que quiero probar con github")
-#genial, otro comentario innecesario
-asd = 91
-#str es para concatenar otro tipo de variables que no sean string
-print("La edad de tu gfa: "+str(asd))
 
-print("Tu mamá es tan gorda que si se cae le da la vuelta al mundo "+str(asd)+" veces")
 
-#Officia et qui pariatur elit nisi commodo reprehenderit cillum Lorem est.
-print("Culpa qui excepteur ut nisi ullamco nulla non consectetur id culpa incididunt. Deserunt voluptate aliquip laborum minim cupidatat quis amet minim. Ex exercitation amet nostrud sit non proident nisi. Fugiat cillum nulla veniam culpa reprehenderit voluptate dolore incididunt non id sit. Occaecat eu occaecat velit duis labore anim sunt ut laboris veniam elit et sint. Anim laboris aute dolore adipisicing. Aliqua id Lorem ullamco ex minim sit commodo cupidatat ea.")
+# print("Cosas que quiero probar con github")
+# #genial, otro comentario innecesario
+# asd = 91
+# #str es para concatenar otro tipo de variables que no sean string
+# print("La edad de tu gfa: "+str(asd))
+
+# print("Tu mamá es tan gorda que si se cae le da la vuelta al mundo "+str(asd)+" veces")
+
+# #Officia et qui pariatur elit nisi commodo reprehenderit cillum Lorem est.
+# print("Culpa qui excepteur ut nisi ullamco nulla non consectetur id culpa incididunt. Deserunt voluptate aliquip laborum minim cupidatat quis amet minim. Ex exercitation amet nostrud sit non proident nisi. Fugiat cillum nulla veniam culpa reprehenderit voluptate dolore incididunt non id sit. Occaecat eu occaecat velit duis labore anim sunt ut laboris veniam elit et sint. Anim laboris aute dolore adipisicing. Aliqua id Lorem ullamco ex minim sit commodo cupidatat ea.")
+
+
+
+print("A ver que pasa con los nuevos cambios que voy a hacer")
+a,b,c = 10,20,30
+print("suma de 10 en 10: "+str(a)+"+10= "+str(b)  )
