c67339d4ca90d6c7c1665b2f9739de71185bace9
Erick Bautista <72156877+oxjleoxo@users.noreply.github.com>
2022-06-14 09:33:09 -0500

    Reto incompleto de números primos
    Falta hacer que el resultado sea el correcto

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,41 +1,3 @@
-"""
-from tkinter import ttk
-from tkinter import *
-
-import sqlite3
-
-class Productos:
-    def __init__(self, root):
-        self.wind = root
-        self.wind.title("Productos")
-        self.wind.geometry("900x600")
-
-        frame1 = LabelFrame(self.wind, text="Información del Producto", font=("Calibri",14))
-        frame2 = LabelFrame(self.wind, text="Datos del Producto", font=("Calibri",14))
-
-        frame1.pack(fill="both", expand="yes", padx=20, pady=10)
-        frame2.pack(fill="both", expand="yes", padx=20, pady=10)
-
-        self.trv = ttk.Treeview(frame1,columns=(1,2,3,4), show="headings", height="5")
-        self.trv.pack()
-
-        self.trv.heading(1, text="ID del Producto")
-        self.trv.heading(2, text="Nombre del producto")
-        self.trv.heading(3, text="Precio del Producto")
-        self.trv.heading(4, text="Cantidad del producto")
-    
-
-
-if __name__ == '__main__':
-    root = Tk()
-    product = Productos(root)
-    root.mainloop()
-
-"""
-
-
-
-
 # #el type() funciona para mostrar el tipo de variable que es una variable
 # print(type("Cosas simples"))
 # #el + funciona para unir cadenas de texto y en numeros los suma 
@@ -77,43 +39,48 @@ if __name__ == '__main__':
 # GRAVITY = 3.14
 # print(type(GRAVITY))
 
-
-
-
-
-# print("Cosas que quiero probar con github")
-# #genial, otro comentario innecesario
-# asd = 91
-# #str es para concatenar otro tipo de variables que no sean string
-# print("La edad de tu gfa: "+str(asd))
-
-# print("Tu mamá es tan gorda que si se cae le da la vuelta al mundo "+str(asd)+" veces")
-
-# #Officia et qui pariatur elit nisi commodo reprehenderit cillum Lorem est.
-# print("Culpa qui excepteur ut nisi ullamco nulla non consectetur id culpa incididunt. Deserunt voluptate aliquip laborum minim cupidatat quis amet minim. Ex exercitation amet nostrud sit non proident nisi. Fugiat cillum nulla veniam culpa reprehenderit voluptate dolore incididunt non id sit. Occaecat eu occaecat velit duis labore anim sunt ut laboris veniam elit et sint. Anim laboris aute dolore adipisicing. Aliqua id Lorem ullamco ex minim sit commodo cupidatat ea.")
-
-
-<<<<<<< HEAD
+# end="" funciona para evitar que se haga un salto de linea, para cuando se tienen que imprimir varias cosas en la misma linea
+# print("Caso de prueba "+str(n4),end="")
+# print(":  numero: "+str(n3))
 
 
 
 
 
+con1=True
+while con1 == True:
+    try:
+        casosDePrueba=int(input("Cantidad de casos de prueba: "))
+    except Exception:
+        print("No es un número admitido")
+    else:
+        con1=False
+        n4=0
+        # n4 = contador de numeros de pruebas -Eliminar al terminar
+        while n4<casosDePrueba:
+            try:
+                n3=int(input("Numero de Prueba "+str(n4+1)+": "))
+            except Exception:
+                print("No es un numero admitido")
+            else:
+                n4+=1
+                
+                i=2
+                modulo=2
+                print(str(n3)+" = ",end="")
 
-
-
-
-
-
-
-
-
-
-
-
-=======
-print("lorem ipsum ")
-print("Prueba la nuev tecnología Powershll multiplataforma https://aka.ms/pscore6 "/
-    " PS C:\MAMP\htdocs> &")
-sexo="sexoooooooooooooooooooooooo"
->>>>>>> cambios-con-texto
+                while modulo > 1:
+                    modulo=i%n3
+                    
+                    if (modulo%i)<1 :
+                        i+=1
+                    else:
+                        
+                        n3=round(n3/i)
+                        
+                        print(" Modulo: "+str(modulo),end="")
+                        print(" Resultado: "+str(n3)+", ",end="")    
+                print("")    
+                    
+                # print("Caso de prueba "+str(n4),end="")
+                # print(":  numero: "+str(n3))
