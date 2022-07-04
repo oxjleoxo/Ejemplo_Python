b9b51571ef7d2b328fe3479076436e363ea6c47d
Erick Bautista <72156877+oxjleoxo@users.noreply.github.com>
2022-07-04 09:59:30 -0500

    ultimacosa

--- Ejercicios.py
+++ Ejercicios.py
@@ -14,11 +14,14 @@
 # #listas
 # lista1 = [1,2,3,4]
 # print(type(lista1))
-# #tuplas, son listas pero que no se pueden cambiar los datos
-# dupla1 = (10,20,30,40)
-# print(type(dupla1))
+#tuplas, son listas pero que no se pueden cambiar los datos
+from os import dup
 
 
+dupla1 = (10,20,30,40)
+print(type(dupla1))
+print(dupla1)
+print(dupla1[0])
 # #diccionarios
 # diccionario1 = {
 #     "Nombre de la persona":"Ryan",
@@ -47,40 +50,62 @@
 
 
 
-con1=True
-while con1 == True:
-    try:
-        casosDePrueba=int(input("Cantidad de casos de prueba: "))
-    except Exception:
-        print("No es un número admitido")
-    else:
-        con1=False
-        n4=0
-        # n4 = contador de numeros de pruebas -Eliminar al terminar
-        while n4<casosDePrueba:
-            try:
-                n3=int(input("Numero de Prueba "+str(n4+1)+": "))
-            except Exception:
-                print("No es un numero admitido")
-            else:
-                n4+=1
-                
-                i=2
-                modulo=2
-                print(str(n3)+" = ",end="")
 
-                while modulo > 1:
-                    modulo=i%n3
-                    
-                    if (modulo%i)<1 :
-                        i+=1
-                    else:
-                        
-                        n3=round(n3/i)
-                        
-                        print(" Modulo: "+str(modulo),end="")
-                        print(" Resultado: "+str(n3)+", ",end="")    
-                print("")    
-                    
-                # print("Caso de prueba "+str(n4),end="")
-                # print(":  numero: "+str(n3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# con1=True
+# while con1 == True:
+#     try:
+#         casosDePrueba=int(input("Cantidad de casos de prueba: "))
+#     except Exception:
+#         print("No es un número admitido")
+#     else:
+#         con1=False
+#         n4=0
+#         # n4 = contador de numeros de pruebas -Eliminar al terminar
+#         while n4<casosDePrueba:
+#             try:
+#                 n3=int(input("Numero de Prueba "+str(n4+1)+": "))
+#             except Exception:
+#                 print("No es un numero admitido")
+#             else:
+#                 n4+=1
+#                 print(str(n3)+"= ",end="")
+#                 for i in range(2,9999999999999999):
+#                     if i <= n3:
+#                         while (n3%i)==0:
+#                             print(str(i)+" ",end="" )
+#                             n3 = round(n3/i)
+#                     else:
+#                         break
+#                 print("")    
+
+
+
+
+
+
+# exit()
+
+
+
+
+
+
